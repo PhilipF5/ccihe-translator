c136c4f1abdebdb7d46277ca0fe5d0c82b234004
Philip Fulgham <philfulgham@gmail.com>
2017-10-28 14:27:15 -0400

    feat: handle shared legend for BASIC2005/BASIC2010

--- app.js
+++ app.js
@@ -27,22 +27,30 @@ for (let row = FIRST_ROW; row < 270; row++) {
 				"update": { "$set": {} }
 			}
 		};
-		op.updateMany.filter[variable] = value_cell.v;
-		if (variable.endsWith("FLAG")) {
-			op.updateMany.update.$set[variable] = Boolean(value_cell.v);
-		}
-		else {
-			switch (labelsWs[LABEL_COLUMN + row].v) {
-				case "No":
-					op.updateMany.update.$set[variable] = false;
-					break;
-				case "Yes":
-					op.updateMany.update.$set[variable] = true;
-					break;
-				default:
-					op.updateMany.update.$set[variable] = labelsWs[LABEL_COLUMN + row].v;
-					break;
-			}
+		switch (variable) {
+			case "BASIC2005":
+				op.updateMany.filter = { "$or": [{ "BASIC2005": value_cell.v }, { "BASIC2010": value_cell.v }] };
+				op.updateMany.update.$set["BASIC2005"] = op.updateMany.update.$set["BASIC2010"] = labelsWs[LABEL_COLUMN + row].v;
+				break;
+			default:
+				op.updateMany.filter[variable] = value_cell.v;
+				if (variable.endsWith("FLAG")) {
+					op.updateMany.update.$set[variable] = Boolean(value_cell.v);
+				}
+				else {
+					switch (labelsWs[LABEL_COLUMN + row].v) {
+						case "No":
+							op.updateMany.update.$set[variable] = false;
+							break;
+						case "Yes":
+							op.updateMany.update.$set[variable] = true;
+							break;
+						default:
+							op.updateMany.update.$set[variable] = labelsWs[LABEL_COLUMN + row].v;
+							break;
+					}
+				}
+				break;
 		}
 		writeOps.push(op);
 		value_cell = labelsWs[VALUE_COLUMN + ++row];
